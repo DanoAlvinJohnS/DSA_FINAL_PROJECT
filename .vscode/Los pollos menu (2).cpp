09dbfddb55c37d930494eb1142e15f906be33fd0
AkiraEscalona <qadafescalona@tip.edu.ph>
2025-11-02 22:53:12 +0800

    Menu combo with question (Not done)
    yeah

--- .vscode/Los pollos menu (2).cpp
+++ .vscode/Los pollos menu (2).cpp
@@ -32,27 +32,27 @@ int main () {
 
     std::cout << "Combo Set 1" << std::endl;
     std::cout << "(" << comboSet1.Food << " + " << comboSet1.Drink << " + " << comboSet1.Side << ")" << std::endl;
-    std::cout << comboSet1.Food_price + comboSet1.Side_price + comboSet1.Drink_price<< "₱" << std::endl;
+    std::cout << comboSet1.Food_price + comboSet1.Side_price + comboSet1.Drink_price<< "?" << std::endl;
     std::cout << std::endl;
 
     std::cout << "Combo Set 2" << std::endl;
     std::cout << "(" << comboSet2.Food << " + " << comboSet2.Drink << " + " << comboSet2.Side << ")" << std::endl;
-    std::cout << comboSet2.Food_price + comboSet2.Side_price + comboSet2.Drink_price<< "₱"<< std::endl;
+    std::cout << comboSet2.Food_price + comboSet2.Side_price + comboSet2.Drink_price<< "?"<< std::endl;
     std::cout << std::endl;
 
     std::cout << "Combo Set 3" << std::endl;
     std::cout << "(" << comboSet3.Food << " + " << comboSet3.Drink << " + " << comboSet3.Side << ")" << std::endl;
-    std::cout << comboSet3.Food_price + comboSet3.Side_price + comboSet3.Drink_price<< "₱" << std::endl;
+    std::cout << comboSet3.Food_price + comboSet3.Side_price + comboSet3.Drink_price<< "?" << std::endl;
     std::cout << std::endl;
 
     std::cout << "Combo Set 4" << std::endl;
     std::cout << "(" <<comboSet4.Food << " + " << comboSet4.Drink << " + " << comboSet4.Side << ")"<< std::endl;
-    std::cout << comboSet4.Food_price + comboSet4.Side_price + comboSet4.Drink_price<< "₱"<< std::endl;
+    std::cout << comboSet4.Food_price + comboSet4.Side_price + comboSet4.Drink_price<< "?"<< std::endl;
     std::cout << std::endl;
 
     std::cout << "Combo Set 5" << std::endl;
     std::cout << "(" << comboSet5.Food << " + " << comboSet5.Drink << " + " << comboSet5.Side << ")" << std::endl;
-    std::cout << comboSet5.Food_price + comboSet5.Side_price + comboSet5.Drink_price<< "₱" << std::endl;
+    std::cout << comboSet5.Food_price + comboSet5.Side_price + comboSet5.Drink_price<< "" << std::endl;
     std::cout << std::endl;
 
 
